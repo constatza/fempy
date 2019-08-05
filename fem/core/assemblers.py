53ce3bc2490504d68d82542a062713bdd7e520a8
Constantinos <const.atza@gmail.com>
2019-08-05 18:15:08 +0300

    method addition
    Added: calculate_global_matrix

--- fem/core/assemblers.py
+++ fem/core/assemblers.py
@@ -4,7 +4,7 @@ Created on Tue Jul 30 13:49:31 2019
 
 @author: constatza
 """
-from numpy import empty, nanmin, NaN
+from numpy import empty, nanmin, NaN, zeros
 
 
 class ProblemStructural:
@@ -12,24 +12,24 @@ class ProblemStructural:
     
     def __init__(self, model):
         self.model = model
-        self._matrix2D = None
+        self._matrix = None
 
     @property
-    def matrix2D(self):
-        if self._matrix2D == None:
+    def matrix(self):
+        if self._matrix == None:
             self.build_matrix()
         else:
             self.rebuild_matrix()
-        return self._matrix2D
+        return self._matrix
     
     def build_matrix(self):
         """ Builds the global Stiffness Matrix"""
         provider = ElementStructuralStiffnessProvider()
-        self._matrix2D = GlobalMatrixAssembler.CalculateGlobalMatrix(model, provider)
+        self._matrix = GlobalMatrixAssembler.calculate_global_matrix(model, provider)
     
     def rebuild_matrix(self):
         """ Rebuilds the global Stiffness Matrix"""
-        self._matrix2D = GlobalMatrixAssembler.CalculateGlobalMatrix(model, provider)
+        self._matrix = GlobalMatrixAssembler.calculate_global_matrix(model, provider)
     
     def calculate_matrix(self, linear_system):
         if self._matrix2D == None:
@@ -42,7 +42,8 @@ class ElementStructuralStiffnessProvider:
     Responsible for providing elemental stiffness matrix 
     for the global matrix assembly.
     """
-    def matrix2D(element):
+    @staticmethod
+    def matrix(element):
         return element.element_type.stiffness_matrix(element)
 
 
@@ -71,5 +72,58 @@ class GlobalMatrixAssembler:
                     
                     if dof != -1:
                         minDOF = nanmin(minDOF, dof)
-                    
     
+    @staticmethod           
+    def calculate_global_matrix(model, nodal_DOFs_dictionary, element_provider):
+        """Calculates the global stiffness matrix.
+       
+        Parameters
+        ----------
+        model : Model
+            The model whose matrix is to be calculated.
+        nodal_DOFs_dictionary : dict<int, dict<DOFType, int>>
+            Dictionary that links node.ID and DOFType with the equivalent 
+            global nodal DOF number.
+        element_provider : ElementStructuralStiffnessProvider
+            The element provider.
+        
+        Returns
+        -------
+        global_stiffness_matrix : np.ndarray
+            Model global stiffness matrix.
+        """
+        
+        
+        numDOFs = model.total_DOFs
+        global_stiffness_matrix = zeros([numDOFs, numDOFs])
+        
+        for element in model.elements:
+            
+            element_matrix = element_provider.matrix(element)
+            element_DOFtypes = element.element_type.DOF_enumerator.get_DOF_types(element)
+            matrix_assembly_nodes = element.element_type.DOF_enumerator.get_nodes_for_matrix_assembly(element)
+            
+            element_matrix_row = 0
+            for i in range(len(element_DOFtypes)):
+                node_row = matrix_assembly_nodes[i]
+                for DOFtype_row in element_DOFtypes[i]:
+                    DOFrow = nodal_DOFs_dictionary[node_row.ID][DOFtype_row]
+                    
+                    if DOFrow != -1:
+                        
+                        element_matrix_column = 0  
+                        for j in range(i,len(element_DOFtypes)):
+                            node_column = matrix_assembly_nodes[j]
+                            for DOFtype_column in element_DOFtypes[j]:
+                                DOFcolumn = nodal_DOFs_dictionary[node_column.ID][DOFtype_column]
+                                if DOFcolumn != -1:
+                                    global_stiffness_matrix[DOFrow, DOFcolumn] += element_matrix[element_matrix_row,
+                                                                                                 element_matrix_column]
+                                element_matrix_column += 1
+                    
+                    element_matrix_row += 1
+        
+        return global_stiffness_matrix 
+                
+            
+        
