3b5dab9ee41a49e1c8fdd1ef9ab07d55fcfb9253
Constantinos <const.atza@gmail.com>
2019-08-21 17:36:08 +0300

    added sparse Solvers
    Performance improvement

--- fem/core/solvers.py
+++ fem/core/solvers.py
@@ -1,6 +1,8 @@
 
 
-import scipy.linalg as linalg
+from scipy import linalg, sparse
+from scipy.sparse import linalg as splinalg
+
 
 class Solver:
     
@@ -15,13 +17,30 @@ class Solver:
 class SimpleSolver(Solver):
     
     def __init__(self, linear_system):
-        Solver.__init__(self, linear_system)
+        super().__init__(linear_system)
 
     def solve(self):
+       # S = sparse.bsr_matrix(self.system.matrix)
         L = linalg.cho_factor(self.system.matrix)
         self.system.solution = linalg.cho_solve(L, self.system.rhs)
 
+class SparseSolver(Solver):
+    def __init__(self, linear_system):
+        super().__init__(linear_system)
+    
+    def solve(self):
+        sparseM = sparse.csr_matrix(self.system.matrix)
+        self.system.solution = splinalg.spsolve(sparseM, self.system.rhs)
 
+class SparseLUSolver(Solver):
+    def __init__(self, linear_system):
+        super().__init__(linear_system)
+    
+    def solve(self):
+        sparseM = sparse.csc_matrix(self.system.matrix)
+        solveLU = splinalg.factorized(sparseM)
+        self.system.solution = solveLU(self.system.rhs)
+     
 class LinearSystem:
     """
     This class represents a linear system as a collection
