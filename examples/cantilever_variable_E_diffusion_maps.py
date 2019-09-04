c81f66c08e030735baf7faac811a91a0c5c60061
Constantinos <const.atza@gmail.com>
2019-09-04 10:48:10 +0300

    clear old

--- examples/cantilever_variable_E_diffusion_maps.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 26 10:17:36 2019
-
-@author: constatza
-"""
-
--- examples/untitled1.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul  6 20:25:30 2019
-
-@author: constatza
-"""
-import test
