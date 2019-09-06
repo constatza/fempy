12b3167ab32023eb472ff6eaf003bbf204604c5f
Constantinos <const.atza@gmail.com>
2019-09-06 13:20:04 +0300

    Update variable_young_modulus_results.py

--- examples/variable_young_modulus/variable_young_modulus_results.py
+++ examples/variable_young_modulus/variable_young_modulus_results.py
@@ -10,32 +10,36 @@ import matplotlib.pyplot as plt
 import fempy.dmaps as dm
 from mpl_toolkits.mplot3d import Axes3D
 
-filename = r"variable_E_displacements_20x50.npy"
+"""
+Input
+"""
+epsilon = 1
+numeigs = 100
+timesteps = 1
+Nsim = 2000
 
+filename = r"variable_E_displacements_20x50.npy"
 with open(filename) as file:
     displacements = np.load(filename)
 
 displacements = np.transpose(displacements)
-
-
-epsilon = 5
-numeigs = 10
-timesteps = 1
-Nsim = 10
+U = displacements[:, :Nsim]
+utop = displacements[:,:]
+utop_norm1 = np.linalg.norm(utop, 1, axis=0)
 
 eigvals, eigvecs = dm.diffusion_maps(displacements[:,:Nsim], epsilon=epsilon, t=timesteps, k=numeigs)
-Fi = -eigvals* eigvecs
+Fi = eigvals* eigvecs
 
-
-k = len(eigvals[eigvals>0.1])
+k = len(eigvals[eigvals>0.05])
 A, res = dm.ls_approx(displacements[:, :Nsim], Fi[:,:k])
 
 print(A.shape)
 
 Unew = A @ Fi[:, :k].T
-xnew = Unew[0,:]
-ynew = Unew[1,:]
-znew = Unew[2,:]
+
+errors = Unew - U
+errnorm = np.linalg.norm(errors)
+relative_err = (errors/U)
 
 #epsilons = np.logspace(-3,3)
 #M = dm.M(displacements, epsilon=epsilons)
@@ -47,18 +51,6 @@ plt.plot(eigvals, 'o-')
 plt.ylabel('eigenvalues')
 plt.grid()
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111, projection='3d')
-ax2.scatter(Fi[:,1], Fi[:,2], Fi[:,3])
-plt.grid()
-plt.ylabel('Ψ2')
-plt.xlabel('Ψ1')
-plt.show() 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(displacements[0, :Nsim],displacements[1, :Nsim], displacements[2, :Nsim])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.scatter(xnew,ynew,znew, color='g')     
+
+   
