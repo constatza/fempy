67753fb3b3f5fa750ce38561d670a2439966366d
Constantinos <const.atza@gmail.com>
2019-10-01 13:24:25 +0300

    added eigenvector_plot and hist

--- smartplot.py
+++ smartplot.py
@@ -4,8 +4,13 @@ Created on Tue Sep 24 17:56:44 2019
 
 @author: constatza
 """
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
+def histogram(x, *args, **kwargs):
+    sns.set()
+    return sns.distplot(x, *args, **kwargs)
 
 def formal_serif():    
     plt.rc( 'text', **{'usetex' : True})
@@ -18,3 +23,40 @@ def formal_serif():
     plt.rc('xtick', labelsize=12)
     plt.rc('ytick', labelsize=12)
     plt.rc('axes', labelsize=12)
+
+
+def eigenvector_plot(F, title=None,label=None):
+    numeigs = F.shape[1]
+    fig = plt.figure()
+    if numeigs==2:
+        
+        ax = fig.add_subplot(111)
+        lines = ax.scatter(F[:, 0], F[:, 1])
+        ax.set_xlabel('$\psi_1$')
+        ax.set_ylabel('$\psi_2$')
+
+    elif numeigs==3:
+        ax = fig.add_subplot(111, projection='3d')
+        lines = ax.scatter(F[:,0], F[:, 1], F[:, 2])
+        ax.set_xlabel('$\psi_1$')
+        ax.set_ylabel('$\psi_2$')
+        ax.set_zlabel('$\psi_3$')
+        
+    else:
+        ax = fig.add_subplot(111)
+        lines = ax.plot(F)
+        ax.legend(lines, list(np.arange(F.shape[1])+1))
+
+    ax.set_title(title)
+    ax.grid()
+    return ax
+
+if __name__ == "__main__":
+    plt.close('all')
+    formal_serif()
+    F = np.reshape(np.arange(20), (5,-1) )   
+    
+    ax = eigenplot(F, title='Nice')
+    
+    plt.show()
+
