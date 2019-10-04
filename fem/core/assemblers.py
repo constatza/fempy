a307ef0ea88296d78e09d414d4e982d92191362d
Constantinos <const.atza@gmail.com>
2019-10-04 14:58:24 +0300

    Update assemblers.py

--- fem/core/assemblers.py
+++ fem/core/assemblers.py
@@ -10,6 +10,7 @@ from numpy import array, zeros, newaxis, arange, empty
 from scipy import sparse
 from numba import njit, prange, parfor
 
+
 class GenericDOFEnumerator:
     """Retrieves element connectivity data required for matrix assembly."""
     
@@ -49,7 +50,8 @@ class ProblemStructural:
     def __init__(self, model):
         self.model = model
         self._matrix = None
-        self.stiffness_provider = ElementStructuralStiffnessProvider()
+        self.stiffness_provider = ElementStiffnessProvider()
+        self.mass_provider = ElementMassProvider()
 
     @property
     def matrix(self):
@@ -61,7 +63,7 @@ class ProblemStructural:
     
     def build_matrix(self):
         """ Builds the global Stiffness Matrix"""
-        provider = ElementStructuralStiffnessProvider()
+        provider = ElementStiffnessProvider()
         self._matrix = GlobalMatrixAssembler.calculate_global_matrix(self.model, provider)
     
     def rebuild_matrix(self):
@@ -70,16 +72,73 @@ class ProblemStructural:
     
     def calculate_matrix(self, linear_system):
         linear_system.matrix = self.matrix
-        
-        
 
-class ElementStructuralStiffnessProvider:
+
+class ProblemStructuralDynamic:
+    """Responsible for the assembly of the global stiffness matrix."""
+    
+    def __init__(self, model):
+        self.model = model
+        self._stiffness_matrix = None
+        self._mass_matrix = None
+        self.stiffness_provider = ElementStiffnessProvider()
+        self.mass_provider = ElementMassProvider()
+
+    @property
+    def stiffness_matrix(self):
+        if self._stiffness_matrix is None:
+            self.build_stiffness_matrix()
+        else:
+            self.rebuild_stiffness_matrix()
+        return self._stiffness_matrix
+    
+    @property
+    def mass_matrix(self):
+        if self._stiffness_matrix is None:
+            self.build_mass_matrix()
+        else:
+            self.rebuild_mass_matrix()
+        return self._mass_matrix
+    
+    @stiffness_matrix.setter
+    def stiffness_matrix(self, value):
+        self._stiffness_matrix = value
+    
+    @mass_matrix.setter
+    def mass_matrix(self, value):
+        self._mass_matrix = value
+    
+    def build_stiffness_matrix(self):
+        """ Builds the global Stiffness Matrix"""
+        provider = ElementStiffnessProvider()
+        self.stiffness_matrix = GlobalMatrixAssembler.calculate_global_matrix(self.model, provider)
+        
+    def build_mass_matrix(self):
+        """ Builds the global Mass Matrix"""
+        provider = ElementMassProvider()
+        self.mass_matrix = GlobalMatrixAssembler.calculate_global_matrix(self.model, provider)
+    
+    def rebuild_stiffness_matrix(self):
+        """ Rebuilds the global Stiffness Matrix"""
+        self.stiffness_matrix = GlobalMatrixAssembler.calculate_global_matrix(self.model, self.stiffness_provider)
+
+    def rebuild_mass_matrix(self):
+        """ Rebuilds the global Mass Matrix"""
+        self.stiffness_matrix = GlobalMatrixAssembler.calculate_global_matrix(self.model, self.mass_provider)
+
+    def calculate_matrix(self, linear_system):
+        linear_system.stiffness_matrix = self.stiffness_matrix
+        linear_system.mass_matrix = self.mass_matrix
+
+
+class ElementStiffnessProvider:
     """ Responsible for providing elemental stiffness matrix 
     for the global matrix assembly.
     """
     @staticmethod
     def matrix(element):
         return element.element_type.stiffness_matrix(element)
+
 
 class ElementMaterialOnlyStiffnessProvider:
     """ Responsible for providing elemental stiffness matrix 
@@ -94,7 +153,19 @@ class ElementMaterialOnlyStiffnessProvider:
         element.material.constitutive_matrix = None
         materials = element.materials_at_gauss_points
         return element.calculate_stiffness_matrix(points, materials, thickness)
-    
+
+
+class ElementMassProvider:
+    """ Responsible for providing elemental mass matrix 
+    for the global matrix assembly.
+        
+        Deploys the fact that only the material has changed.
+    """
+    @staticmethod
+    def matrix(element):
+        return element.element_type.mass_matrix(element)
+
+
 class GlobalMatrixAssembler:
     """Assembles the global stiffness matrix."""
     
@@ -110,7 +181,7 @@ class GlobalMatrixAssembler:
         nodal_DOFs_dictionary : dict<int, dict<DOFType, int>>
             Dictionary that links node.ID and DOFType with the equivalent 
             global nodal DOF number.
-        element_provider : ElementStructuralStiffnessProvider
+        element_provider : ElementStiffnessProvider
             The element provider.
         
         Returns
